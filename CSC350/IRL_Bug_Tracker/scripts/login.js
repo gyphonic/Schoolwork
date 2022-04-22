931422fbbca0d505f6b8895c07255f3749784a77
Todd Mills <gyphonic2011@gmail.com>
2022-04-22 21:57:33 +0000

    Small update to Javascript

---
+++ CSC350/IRL_Bug_Tracker/scripts/login.js
@@ -0,0 +1,22 @@
+/*  
+    Todd Mills
+    Week 6 Lab
+    CSC350
+*/
+
+//adds the entered button press to the password value box
+function addValueToPassword(value) {
+
+    if (value = "bksp") {
+        //remove the last character from password
+    } else {
+        //add password value
+    }
+}
+
+//on login button press, check localstoracge for password value and proceed if matching
+function getPassword () {
+    //check localstorage for pass
+    //no pass? disclaimer, else menu page
+
+}
